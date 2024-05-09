2801f030038b42de67b2660cf4f7e0a6a80ac60e
Alishba Fatima <alishbaqureshi09@gmail.com>
2024-05-09 20:00:52 +0500

    first commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import java.util.Random;
+public class Main{
+    public static void main(String[] args){
+        Random number = new Random();
+      int x = number.nextInt(6)+1;
+      System.out.println("you rolled a"+"  " + x);
+    }
+}
